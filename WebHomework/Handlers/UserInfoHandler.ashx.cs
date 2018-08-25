162576dd92f0545381a05b5b4b9692bb6781cd65
name <jopas>
2018-08-26 02:46:11 +0300

    first web homework

---
+++ WebHomework/Handlers/UserInfoHandler.ashx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebHomework.Handlers
+{
+    /// <summary>
+    /// Summary description for UserInfoHandler
+    /// </summary>
+    public class UserInfoHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            string result = "<p>Ваш IP: " + context.Request.UserHostAddress + "</p>";
+            result += "\n<p>UserAgent: " + context.Request.UserAgent + "</p>";
+            context.Response.Write(result);
+            context.Response.WriteFile("abc.abc");
+        }
+
+        public bool IsReusable => false;
+    }
+}
